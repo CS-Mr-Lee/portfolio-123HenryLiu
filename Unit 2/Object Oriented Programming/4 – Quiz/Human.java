8f76e97b643e74add3411714d9421a0c4954d179
123HenryLiu <99341341+123HenryLiu@users.noreply.github.com>
2022-03-02 14:27:03 -0500

    Update Human.java

--- Unit 2/Object Oriented Programming/4 – Quiz/Human.java
+++ Unit 2/Object Oriented Programming/4 – Quiz/Human.java
@@ -7,15 +7,18 @@
  */
 public class Human {
    
-   /** The name of the human */
+  /** The name of the human */
    private String name;
 
-   /** the weight of the human in kg */
+  /** the weight of the human in kilograms */
    private double weight;
 
-   /** the energy level of the human as a percentage from 0 to 100 */
+  /** the energy level of the human as a percentage from 0 to 100 */
    private int energyLevel;
   
+  /**
+   * 
+   */
    public Human() {
    }
   
@@ -39,6 +42,9 @@ public class Human {
       
    }
    
+   
+   // accessor methods
+   
    public String getName() {
       return name;
    }  
@@ -47,21 +53,79 @@ public class Human {
       return weight;
    }  
    
-   public int energyLevel() {
+   public int getEnergyLevel() {
       return energyLevel;
    }  
-
+   
+   
+   //mutator methods
+   
    public void sleep(int hours) {
-      if(hours > 0){
+      if (hours > 0) {
          energyLevel += hours * 10;
       }
    }
 
-   public void run(double km)
-      if(km > 0){
+   public void run(double km) {
+      if (km > 0){
          weight -= 0.001 * km;
          energyLevel -= km * 3;
       }
+   }
+   
+   public void eat(Vegetable veg, double grams) {
+      
+      int calIntakeVeg = veg.eaten(grams);
+      
+      if (calIntakeVeg == -1) {
+         System.out.println("I don’t have that much food");
+      } else {
+         weight += grams / 1000;
+      
+         energyLevel += calIntakeVeg / 15;
+         if (energyLevel > 100) {
+            energyLevel = 100;
+         } else {
+            energyLevel = energyLevel;
+         }
+      }
+   }
+   
+   public void eat(Cookie food, double grams) {
+      
+      int calIntakeCookie = food.eaten(grams);
+      
+      if (calIntakeCookie == -2) {
+         System.out.println("“I can’t eat the bag");
+      } else if (calIntakeCookie == -1) {
+         System.out.println("I don’t have that much food");
+      } else {
+         weight += grams / 1000;
+      
+         energyLevel += calIntakeCookie / 15;
+         if (energyLevel > 100) {
+            energyLevel = 100;
+         } else {
+            energyLevel = energyLevel;
+         }
+      }
+   }
+   
+   public String toString() {
+      
+      String output = "Name: ";
+      output += name;
+      output += "\n";
+      
+      output += "Weight: ";
+      output += weight;
+      output += " kilograms\n";
+      
+      output += "Energy level: ";
+      output += energyLevel;
+      output += "percent\n";
+      
+      return output;
    }
    
 }
