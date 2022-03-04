51930474f3fea3ad07d4a07fe12bc15cd820743d
123HenryLiu <99341341+123HenryLiu@users.noreply.github.com>
2022-03-03 19:37:20 -0500

    Update Liu_Henry_OOQuiz1.java

--- Unit 2/Object Oriented Programming/4 – Quiz/Liu_Henry_OOQuiz1.java
+++ Unit 2/Object Oriented Programming/4 – Quiz/Liu_Henry_OOQuiz1.java
@@ -2,31 +2,25 @@
  * @author Henry Liu
  * Mar 1, 2022
  *
- * This program creates a cookie class
- * 
+ * Main method to run the human, cookie, and vegetable class
  */
 public class Liu_Henry_OOQuiz1 {
    public static void main(String[] args) {
    
-      Human person = new Human("Chris", 50, 88);
-      System.out.println(person.toString());
+      Human hungryPerson = new Human("Chris", 50, 82);
 
       Vegetable purpleVeggie = new Vegetable("Rotten Eggplant", 535.7, 127);
-      System.out.println(purpleVeggie.toString());
       
       Cookie chocoChip = new Cookie("Chips Ahoy Ripoff", -20, -240, false); 
-      System.out.println(chocoChip.toString());
       
       Cookie healthyCookie = new Cookie("Oatmeal Cookie", 15, 90, true); 
-      System.out.println(healthyCookie.toString());
       
-      Cookie perfectDesert = new Cookie("Marshmellow Smores", 22.4, 350, false); 
-      System.out.println(perfectDesert.toString());*/
+      Cookie perfectDesert = new Cookie("Marshmallow S'mores", 22.5, 350, false); 
       
-      person.eat(healthyCookie, 9);
+      System.out.print(hungryPerson.eat(healthyCookie, 9));
       
-      person.eat(purpleVeggie, 550);
-      
-      person.eat(perfectDesert, 18);
+      System.out.print(hungryPerson.eat(purpleVeggie, 550));
+
+      hungryPerson.eat(perfectDesert, 18);
    }
 }
