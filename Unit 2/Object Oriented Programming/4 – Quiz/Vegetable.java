a26613b4950565fe5f3476022bb75bd62805c4d1
123HenryLiu <99341341+123HenryLiu@users.noreply.github.com>
2022-03-03 19:35:33 -0500

    Update Vegetable.java

--- Unit 2/Object Oriented Programming/4 – Quiz/Vegetable.java
+++ Unit 2/Object Oriented Programming/4 – Quiz/Vegetable.java
@@ -2,37 +2,47 @@
  * @author Henry Liu
  * Mar 1, 2022
  *
- * This program creates a cookie class
- * 
+ * This program creates a vegetable class
  */
 public class Vegetable {
+   
+   //attributes
 
-  /** name of the vegetable */
+   /** name of the vegetable */
    private String name;
    
-  /** weight of the vegetable in grams */
+   /** weight of the vegetable in grams */
    private double weight;
 
-  /** amount of calories */
+   /** amount of calories */
    private int calories;
   
-  /** default constructor */
+   //constructors
+   
+   /** default constructor */
    public Vegetable() {
    }
    
   /**
+   * creating the vegetable
    *
+   * @param name, name of the vegetable
+   * @param weight, weight in grams
+   * @param calories, number of calories
    */
    public Vegetable(String name, double weight, int calories) {
       
+      //initializing name
       this.name = name;
       
+      //weight cannot be negative
       if (weight < 0) {
          this.weight = 0;
       } else {
          this.weight = weight;
       }
       
+      //calories cannot be negative
       if (calories < 0) {
          this.calories = 0;
       } else {
@@ -41,23 +51,47 @@ public class Vegetable {
       
    }
    
+   //accessor methods
+   
+  /**
+   * getting the name
+   *
+   * @return name, name of the vegetable
+   */
    public String getName() {
-      return name;
+      return this.name;
    }
    
+  /**
+   * getting the weight
+   *
+   * @return weight, weight of the vegetable in grams
+   */
    public double getWeight() {
-      return weight;
+      return this.weight;
    }
    
+  /**
+   * getting the calories
+   *
+   * @return calories, number of calories of the vegetable
+   */
    public int getCalories() {
-      return calories;
+      return this.calories;
    }
    
    //mutator methods
    
+  /**
+   * eating the vegetable
+   *
+   * @param weight, for the weight eaten
+   * @return caloriesEaten, calories eaten from the vegetable 
+   */
    public int eaten(double weight) {
       int caloriesEaten;
       
+      //weight eaten cannot be more than vegetable's weight
       if (weight > this.weight) {
          caloriesEaten = -1;
       } else {
@@ -68,6 +102,11 @@ public class Vegetable {
       return caloriesEaten;
    }
    
+  /**
+   * initializing toString method
+   *
+   * @return output, outputs all the information
+   */
    public String toString() {
       
       String output = "Name: ";
