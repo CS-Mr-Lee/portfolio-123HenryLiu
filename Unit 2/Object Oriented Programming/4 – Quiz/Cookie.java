3a4cef8024bfc69008af38b655602b5837376a2c
123HenryLiu <99341341+123HenryLiu@users.noreply.github.com>
2022-03-03 19:36:23 -0500

    Update Cookie.java

--- Unit 2/Object Oriented Programming/4 – Quiz/Cookie.java
+++ Unit 2/Object Oriented Programming/4 – Quiz/Cookie.java
@@ -3,74 +3,121 @@
  * Mar 1, 2022
  *
  * This program creates a cookie class
- * 
  */
 public class Cookie {
 
-  /** name of the cookie */
+   //attributes
+
+   /** name of the cookie */
    private String name;
    
-  /** weight of the cookie in grams */
+   /** weight of the cookie in grams */
    private double weight;
 
-  /** amount of calories */
+   /** amount of calories */
    private int calories;
    
-  /** whether it is packaged or not */
+   /** whether it is packaged or not */
    private boolean isPackaged;
    
-  /** default constructor */
+   //constructors
+   
+   /** default constructor */
    public Cookie() {
    }
      
   /** 
-   * @param 
+   * creating the cookie
+   *
+   * @param name, name of the cookie
+   * @param weight, weight of the cookie in grams
+   * @param calories, number of calories
+   * @param isPackaged, cookie is packaged or not
    */
    public Cookie(String name, double weight, int calories, boolean isPackaged) {
    
+      //initializing name
       this.name = name;
    
+      //weight cannot be negative
       if (weight < 0) {
          this.weight = 0;
       } else {
          this.weight = weight;
       }
       
+      //calories cannot be negative
       if (calories < 0) {
          this.calories = 0;
       } else {
          this.calories = calories;
       }
       
+      //initializing package boolean
       this.isPackaged = isPackaged; 
    
    }
    
+   //accessor methods
+   
+  /**
+   * getting the name
+   *
+   * @return name, name of the cookie
+   */
    public String getName() {
-      return name;
+      return this.name;
    }
-   
+
+  /**
+   * getting the weight
+   *
+   * @return weight, weight of the cookie in grams
+   */
    public double getWeight() {
-      return weight;
+      return this.weight;
    }
    
+  /**
+   * getting the calories
+   *
+   * @return calories, number of calories of the vegetable
+   */
    public int getCalories() {
-      return calories;
+      return this.calories;
    }
    
+  /**
+   * getting the package information
+   *
+   * @return isPackaged, says if the cookie is packaged or not
+   */
    public boolean isPackaged() {
-      return isPackaged;
+      return this.isPackaged;
    }
    
    // mutator methods
    
+  /**
+   * opening the cookie
+   * if it's already open, it stays open
+   * if it is closed, it is now open
+   *
+   */
    public void open() {
       isPackaged = false;
    }
    
+  /**
+   * eating the cookie
+   *
+   * @param weight, for the weight eaten
+   * @return caloriesEaten, calories eaten from the cookie 
+   */
    public int eaten(double weight) {
       int caloriesEaten;
       
+      //cannot eat if weight eaten is too high or cookie is packaged
       if (isPackaged = true) {
          caloriesEaten = -2;
       } else if (weight > this.weight) {
@@ -84,6 +131,11 @@ public class Cookie {
    
    }
    
+  /**
+   * initializing toString method
+   *
+   * @return output, outputs all the information
+   */
    public String toString () {
       String output = "Name: ";
       output += name;
